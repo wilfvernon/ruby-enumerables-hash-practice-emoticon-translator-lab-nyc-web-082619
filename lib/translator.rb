4e60b3ea2eea9ac1a98e4605f36f3d2be12d50ed
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-23 12:31:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,19 @@ def load_library(library)
   hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(library, emoticon)
+def load_library(lib)
+  hash = Hash.new
+  hash['get_meaning'] = Hash[YAML.load_file(lib).collect { |item, key| [key[1], item]} ]
+  hash['get_emoticon']= Hash[YAML.load_file(lib).collect { |item, key| [key[0], key[1]]} ]
+  #binding.pry
+  hash
+end
+load_library(library)
+hash['get_emoticon'].each do |key, value|
+  if emoticon == key
+    return value
+end
 end
 
 def get_english_meaning
