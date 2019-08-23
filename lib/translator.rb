8bb7fd50c7106b4a1689ed9cae7413af3ad50abf
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-23 12:23:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"
 def load_library(library)
   hash = Hash.new
   hash['get_meaning'] = Hash[YAML.load_file(library).collect { |item, key| [key[1], item]} ]
-  hash['get_emoticon']= Hash[YAML.load_file(library).collect { |item, key| [item, key[0]]} ]
+  hash['get_emoticon']= Hash[YAML.load_file(library).collect { |item, key| [key[0], item} ]
   binding.pry
   hash
 
